16ff4d8f9958e546549b57bdee702658e29c81dc
Ji Ai <805109684aiji@sjtu.edu.cn>
2023-05-16 02:04:23 +0800

    Update comments net_start_new.py

--- net_start_new.py
+++ net_start_new.py
@@ -1,20 +1,24 @@
 from mininet.cli import CLI
 from mininet.net import Mininet
-from mininet.node import RemoteController
+from mininet.node import RemoteController #远程控制器
 
 
-net = Mininet(controller=RemoteController)
+net = Mininet(controller=RemoteController) # 创建 Mininet 实例，指定控制器类型为 RemoteController
+
 hosts = [net.addHost(f'h{i + 1}', ip=f'223.1.{i + 1}.2/24')
-         for i in range(3)]
-switches = [net.addSwitch(f's{i + 1}') for i in range(3)]
-c0 = net.addController('c0')
+         for i in range(3)]# 创建三个主机，分别设置 IP 地址
+
+switches = [net.addSwitch(f's{i + 1}') for i in range(3)] # 创建三个交换机
+
+c0 = net.addController('c0') # 添加控制器
 
 net.addLink(switches[0], hosts[0], 1, 1)
 net.addLink(switches[1], hosts[1], 1, 1)
-net.addLink(switches[2], hosts[2], 1, 1)
+net.addLink(switches[2], hosts[2], 1, 1)# 将主机和交换机连接
 net.addLink(switches[0], switches[1], 2, 2)
 net.addLink(switches[1], switches[2], 3, 2)
-net.addLink(switches[2], switches[0], 3, 3)
+net.addLink(switches[2], switches[0], 3, 3)# 将交换机之间连接成环状拓扑
+
 
 switches[0].cmd('ifconfig s1-eth1 0')
 switches[0].cmd('ip addr add 223.1.1.1/24 brd + dev s1-eth1')
@@ -35,9 +39,10 @@ switches[2].cmd('ip addr add 223.1.3.1/24 brd + dev s3-eth1')
 switches[2].cmd('ifconfig s3-eth2 0')
 switches[2].cmd('ip addr add 223.1.5.2/24 brd + dev s3-eth2')
 switches[2].cmd('ifconfig s3-eth3 0')
-switches[2].cmd('ip addr add 223.1.6.1/24 brd + dev s3-eth3')
+switches[2].cmd('ip addr add 223.1.6.1/24 brd + dev s3-eth3')# 为交换机和主机配置IP地址
 
-net.start()
+net.start() # 启动网络
+
 
 c0.cmd(
     ''' curl -X POST -d '{"address": "223.1.1.1/24"}' http://localhost:8080/router/0000000000000001 ''')
@@ -56,11 +61,11 @@ c0.cmd(
 c0.cmd(
     ''' curl -X POST -d '{"address": "223.1.5.2/24"}' http://localhost:8080/router/0000000000000003 ''')
 c0.cmd(
-    ''' curl -X POST -d '{"address": "223.1.6.1/24"}' http://localhost:8080/router/0000000000000003 ''')
+    ''' curl -X POST -d '{"address": "223.1.6.1/24"}' http://localhost:8080/router/0000000000000003 ''')# 通过北向 API 将路由器地址告知 SDN 控制器
 
 for i in range(3):
-    hosts[i].cmd(f'ip route add default via 223.1.{i + 1}.1')
+    hosts[i].cmd(f'ip route add default via 223.1.{i + 1}.1') # 添加默认路由
 
-CLI(net)
+CLI(net) # 启动 Mininet CLI
 
-net.stop()
+net.stop() # 停止网络
