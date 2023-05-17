edb6987bb9b744fe1e1c22eaf97e97c3a7ba1efc
xxnanyuan <100514208+xxnanyuan@users.noreply.github.com>
2023-05-17 21:26:38 +0800

    Update add.sh

--- add.sh
+++ add.sh
@@ -1,9 +1,47 @@
 curl -X POST -d '{
-"dpid":"1",
-"match":{
-	"nw_src":"223.1.1.2/24",
-	"nw_dst":"223.1.2.2/24",
-	"in_port":1
-},
-"actions":[{"type":"OUTPUT","port":2}]
+	"dpid":"1",
+    "priority": 2000,
+    "flags": 1,
+	"match":{
+		"VLAN_ID":100,
+		"IP_DST":"223.1.2.2/24",
+		"in_port":1
+	},
+	"actions":[{"type":"OUTPUT","port":2}]
+}' http://127.0.0.1:8080/stats/flowentry/add
+
+curl -X POST -d '{
+	"dpid":"1",
+    "priority": 2000,
+    "flags": 1,
+	"match":{
+		"VLAN_ID":100,
+		"IP_DST":"223.1.1.2/24",
+		"in_port":2
+	},
+	"actions":[{"type":"OUTPUT","port":1}]
+}' http://127.0.0.1:8080/stats/flowentry/add
+
+curl -X POST -d '{
+	"dpid":"2",
+    "priority": 2000,
+    "flags": 1,
+	"match":{
+		"VLAN_ID":100,
+		"IP_DST":"223.1.2.2/24",
+		"in_port":1
+	},
+	"actions":[{"type":"OUTPUT","port":2}]
+}' http://127.0.0.1:8080/stats/flowentry/add
+
+curl -X POST -d '{
+	"dpid":"2",
+    "priority": 2000,
+    "flags": 1,
+	"match":{
+		"VLAN_ID":100,
+		"IP_DST":"223.1.1.2/24",
+		"in_port":2
+	},
+	"actions":[{"type":"OUTPUT","port":1}]
 }' http://127.0.0.1:8080/stats/flowentry/add
