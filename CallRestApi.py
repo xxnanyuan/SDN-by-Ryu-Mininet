c9abb0cac6c7f8464690e21ef6e457c30d77df55
xxnanyuan <100514208+xxnanyuan@users.noreply.github.com>
2023-05-18 22:00:20 +0800

    Update comment

--- CallRestApi.py
+++ CallRestApi.py
@@ -1,27 +1,40 @@
+# 此文件将rest api的调用封装起来
+# 关于rest api的具体情况请参考https://github.com/faucetsdn/ryu/blob/master/ryu/app文件夹下ofctl_rest.py、rest_router.py、rest_topology.py
 import requests 
 import json
- 
+
+# 获取所有交换机信息
 def get_all_switches():
     url = "http://127.0.0.1:8080/v1.0/topology/switches"
     req = requests.get(url).json()
     return req
- 
+   
+# 获取所有链路信息 
 def get_all_links():
     url = "http://127.0.0.1:8080/v1.0/topology/links"
     req = requests.get(url).json()
     return req
- 
+   
+# 获取dpid为dpid（可以理解为交换机序号）的交换机的所有流表，此处dpid不为16位
 def get_flow_entries(dpid):
     url = "http://127.0.0.1:8080/stats/flow/" + dpid
     req = requests.get(url).json()
     return req
- 
+   
+# 给dpid为dpid（可以理解为交换机序号）的交换机添加一个流表项，
+# 关于流表项的具体设置：
+# priority默认设置为1000
+# match中目的地ip设置应为一个子网中所有ip，即1.0.0.0/24，最后一位为0（参照http://osrg.github.io/ryu-book/en/html/rest_router.html），其他项请自行查阅文档
+# actions的设置：action主要有两种，一是output，附带端口，二是drop，即丢包
+# 此处dpid不为16位
 def add_flow_entry(dpid,match,priority,actions):
     url = "http://127.0.0.1:8080/stats/flowentry/add"
     post_data = "{'dpid':%s,'match':%s,'priority':%s,'actions':%s}" % (dpid,str(match),priority,str(actions))
     req = requests.post(url,data=post_data)
     return req.status_code
- 
+   
+# 删除一个流表项
+# 本项目中用不到
 def delete_flow_entry(dpid, match=None, priority=None, actions=None):
     url = "http://127.0.0.1:8080/stats/flowentry/delete"
     post_data = "{'dpid':%s" % dpid
@@ -35,8 +48,8 @@ def delete_flow_entry(dpid, match=None, priority=None, actions=None):
     req = requests.post(url,data=post_data)
     return req.getcode()
 
+# 获取路由信息
 def get_router(dpid):
     url = "http://localhost:8080/router/"+dpid
     req = requests.get(url).json()
     return req
-#add_flow_entry("2",match4,priority,actions4)
