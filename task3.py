479e923339aa948f6cb89c48927c2f42fb73321b
Loeyxxx <108788206+Loeyxxx@users.noreply.github.com>
2023-05-23 17:43:48 +0800

    Add files via upload

--- task3.py
+++ task3.py
@@ -2,13 +2,38 @@
 Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
 Date: 2023-05-22 14:09:24
 LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
-LastEditTime: 2023-05-23 16:22:22
+LastEditTime: 2023-05-23 17:14:59
 FilePath: \project\SDN-by-Ryu-Mininet-master\task3.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 import time
 import CallRestApi
 from dijk import dijk_g
+
+#传1表示3(a)功能实现
+def proto_valid(n,dst):
+  if(n==1):
+    if(int(dst)==2):
+      match = {"nw_dst":"223.1.%s.0/24"%dst,"dl_type":2048,"nw_proto":6}
+      print("match here:",match)
+    else:
+      match={"nw_dst":"223.1.%s.0/24"%dst,"dl_type":2048}
+  else:
+    match={"nw_dst":"223.1.%s.0/24"%dst,"dl_type":2048}
+  return match
+
+#传1表示3(b)功能实现
+def ipdst_valid(n,LS_table,output_port,src,dst):
+  if(n==1):
+    match={"nw_dst":"223.1.3.0/24","dl_type":2048}
+    print(output_port,LS_table,src,dst)
+    print(output_port[src+LS_table[src][dst]])
+    actions=[{"type":"SET_FIELD","field":"ipv4_dst","value":"223.1.1.2"},{"type":"OUTPUT","port":int(output_port[src+LS_table[src][dst]])}]
+    CallRestApi.modify_flow_entry(str(2),match,2000,actions)
+  else:
+    return
+  
+
 # while true和time.sleep(5)结构配合使得每5秒计算一次静态路由，发送给交换机
 while True:
     switches={} # 交换机信息
@@ -18,9 +43,7 @@ while True:
     
     # 下面通过调用rest api获取相关信息
     for switch in CallRestApi.get_all_switches():
-        print("Switch:",switch)
         router=CallRestApi.get_router(switch["dpid"])
-        print("Router:",router)
         dpids.append(switch["dpid"])
         switches[switch["dpid"]]={port["name"]:router[0]["internal_network"][0]["address"][int(port["name"][-1])-1]["address"] for port in switch["ports"]}
     for item in CallRestApi.get_all_links():
@@ -45,18 +68,11 @@ while True:
         for dst in LS_table[src]:
             if LS_table[src][dst]==-1:
                 continue
-            if(int(dst)==2):
-               #只允许TCP流量发送到h2
-               match = {"nw_dst":"223.1.%s.0/24"%dst,"dl_type":2048,"nw_proto":6}
-            else:
-              match={"nw_dst":"223.1.%s.0/24"%dst,"dl_type":2048}
+            match=proto_valid(1,dst)
             actions=[{"type":"OUTPUT","port":int(output_port[src+LS_table[src][dst]])}]
             CallRestApi.add_flow_entry(src,match,1000,actions)
     
-    #h2发出的流量全部发往h1
-    match={"nw_dst":"223.1.3.0/24","dl_type":2048}
-    actions=[{"type":"SET_FIELD","field":"ipv4_dst","value":"223.1.1.2"},{"type":"OUTPUT","port":2}]
-    CallRestApi.modify_flow_entry(str(2),match,2000,actions)
+    ipdst_valid(1,LS_table,output_port,str(2),str(1))
     
     time.sleep(5)
         
