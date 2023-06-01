b19fdde5520e81aef28403a3e41d64e8756c461d
Ji Ai <805109684aiji@sjtu.edu.cn>
2023-06-02 04:36:21 +0800

    Update firewall.py

--- firewall.py
+++ firewall.py
@@ -5,7 +5,7 @@ from dijk import dijk_g
 
 # 防火墙和入侵检测功能
 # 规则1 拒绝来自 h1（IP 地址为 223.1.1.2）的数据包
-# 规则2 拒绝发送到 h2（IP 地址为 223.1.1.3）的数据包
+# 规则2 拒绝发送到 h1（IP 地址为 223.1.1.2）的数据包
 # 规则3 拒绝传输层协议为 TCP、载荷包含hack 的数据包
 
 switches = {}
@@ -14,19 +14,19 @@ output_port = {}
 dpids = []
 
 
-# 调用rest api s_1, s_2, s_3
+
+# 获取了链路信息，存在 net 字典
 for switch in CallRestApi.get_all_switches():
     router = CallRestApi.get_router(switch["dpid"])
     dpids.append(switch["dpid"])
     switches[switch["dpid"]] = {port["name"]: router[0]["internal_network"][0]["address"][int(
         port["name"][-1])-1]["address"] for port in switch["ports"]}
-# 获取switch和router的信息，存在 switches 字典
 
+# 获取switch和router的信息，存在 switches 字典
 for item in CallRestApi.get_all_links():
     net[item["src"]["name"]] = item["dst"]["name"]
     output_port[item["src"]["name"][1]+item["dst"]
                 ["name"][1]] = int(item["src"]["name"][-1])
-# 获取了链路信息，存在 net 字典
 
 cst = 1
 topo_graph = {}
@@ -45,15 +45,15 @@ for src in LS_table:
     actions = []
     CallRestApi.add_flow_entry(src, match, 1000, actions)
 
-    match = {"nw_dst": "223.1.1.3", "dl_type": 2048}  # 拒绝发送到h2的数据包
+    match = {"nw_dst": "223.1.1.2", "dl_type": 2048}  # 拒绝发送到h1的数据包
     actions = []
     CallRestApi.add_flow_entry(src, match, 1000, actions)
 
     match = {"dl_type": 2048, "nw_proto": 6, "tcp_payload": "hack"}
     actions = []
-    CallRestApi.add_flow_entry(src, match, 1000, actions)
+    CallRestApi.add_flow_entry(src, match, 1000, actions)   # 拒绝TCP 载荷包含关键字 "hack"的数据包
 
   # 匹配协议类型为 IPv4（"dl_type": 2048）
   # 传输层协议类型为 TCP（"nw_proto": 6）
   # 三个防火墙规则
-  # TCP 载荷包含关键字 "hack"
+
